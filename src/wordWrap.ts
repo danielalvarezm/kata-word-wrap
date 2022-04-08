65b538d3e756762bff10f61126ceebd764a40a84
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-08 21:08:56 +0100

    Test success: contemplate cases with several lines in the same input

--- src/wordWrap.ts
+++ src/wordWrap.ts
@@ -1,10 +1,15 @@
-export const wordWrap = (lines: string, limit: number): string => {
-  if (lines.length <= limit) return lines;
+const wrapLine = (line: string, limit: number): string => {
+  line = line.trim();
+  if (line.length <= limit) return line;
 
-  const spaceIndex = lines.slice(0, limit).lastIndexOf(' ');
+  const spaceIndex = line.slice(0, limit).lastIndexOf(' ');
   if (spaceIndex === -1) {
-    return lines.slice(0, limit) + '\n' + wordWrap(lines.slice(limit), limit);    
+    return line.slice(0, limit) + '\n' + wrapLine(line.slice(limit), limit);  
   }
 
-  return lines.slice(0, spaceIndex) + '\n' + wordWrap(lines.slice(spaceIndex + 1), limit);
+  return line.slice(0, spaceIndex) + '\n' + wrapLine(line.slice(spaceIndex + 1), limit);
 };
+
+export const wordWrap = (lines: string, limit: number): string => {
+  return lines.split('\n').map(line => wrapLine(line, limit)).join('\n');
+}
