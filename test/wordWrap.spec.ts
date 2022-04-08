38556443dccb80e6e4d26c53338e12303f7f5b17
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-08 20:52:18 +0100

    Test fail: contemplate cases with several lines in the same input

--- test/wordWrap.spec.ts
+++ test/wordWrap.spec.ts
@@ -1,18 +1,25 @@
-import { wordWrap } from "../src/wordWrap";
+import { wordWrap } from '../src/wordWrap';
 
-describe("The word wrap kata should", () => {
-  it("not break lines smaller than the limit", () => {
+describe('The word wrap kata should', () => {
+  it('not break lines smaller than the limit', () => {
     expect(wordWrap('Hello world', 15)).toBe('Hello world');
     expect(wordWrap('This is an example', 20)).toBe('This is an example');
   });
 
-  it("break lines longer than the limit", () => {
+  it('break lines longer than the limit', () => {
     expect(wordWrap('Hello', 3)).toBe('Hel\nlo');
     expect(wordWrap('Hellooooo', 3)).toBe('Hel\nloo\nooo');
   });
 
-  it("prefer breaking lines by a space", () => {
+  it('prefer breaking lines by a space', () => {
     expect(wordWrap('Hello word', 7)).toBe('Hello\nword');
     expect(wordWrap('This is an example', 7)).toBe('This\nis an\nexample');
+  });
+
+  it('contemplate cases with several lines in the same input', () => {
+    expect(wordWrap('Hello\nworld', 6)).toBe('Hello\nworld');
+    expect(wordWrap('Hello\nwoooorld', 6)).toBe('Hello\nwoooor\nld');
+    expect(wordWrap('Hello\nsunny world', 7)).toBe('Hello\nsunny\nworld');
+    expect(wordWrap('This is\nan example\ntest', 4)).toBe('This\nis\nan\nexam\nple\ntest');
   });
 });
